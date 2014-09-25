e6c6af547239734a13f0f52882b82b5032941e8d
Justin Zattelman <jzattelman@gmail.com>
2014-09-26 08:39:44 +1000

    final

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Flightlog::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app18550613@heroku.com'],
+  :password       => ENV['jiwmorob'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
