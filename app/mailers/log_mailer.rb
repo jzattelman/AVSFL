256252f28e1f11cc9525c1d04f3c1106684d1891
Justin Zattelman <jzattelman@gmail.com>
2014-09-26 08:06:26 +1000

    final

--- app/mailers/log_mailer.rb
+++ app/mailers/log_mailer.rb
@@ -1,9 +1,9 @@
 class LogMailer < ActionMailer::Base
   default from: "flightlog@aerialvisionservices.com"
 
-	def mission_chief_email(mission)
-		@mission = mission
-		@pilot = @mission.remote_pilot
+	def mission_chief_email(session)
+		@session = session
+		@pilot = Pilot.find(@session.remote_pilot_id)
 		mail(to:"justin@aerialvisionservices.com.au", subject: "Flight log - Mission created")
 	end
 end
