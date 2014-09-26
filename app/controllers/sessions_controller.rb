4511792c6f43cd9277c93b8a8acdd48ab694913c
Justin Zattelman <jzattelman@gmail.com>
2014-09-26 10:14:16 +1000

    final

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -134,6 +134,6 @@ class SessionsController < ApplicationController
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
